d6982b1ceec7eb2c478a08c81583d111051d6dfd
AlexisFlop <henryalexislopez@gmail.com>
2024-06-06 22:24:44 -0600

    feature: add Services for Appointment :sparkles:

--- src/main/java/com/kevocodes/pnccontrollers/repositories/AppointmentRepository.java
+++ src/main/java/com/kevocodes/pnccontrollers/repositories/AppointmentRepository.java
@@ -1,11 +1,18 @@
 package com.kevocodes.pnccontrollers.repositories;
 
+import java.util.Date;
 import java.util.UUID;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kevocodes.pnccontrollers.domain.entities.Appointment;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID>{
+
+    @Query(value = "SELECT * from Appointment a WHERE a.endDate = :date")
+    List<Appointment> getAppointmentsByDate(@Param("date") Date thisDate);
     
 }
--- src/main/java/com/kevocodes/pnccontrollers/services/implementations/AppointmentSeriviceImplementation.java
+++ src/main/java/com/kevocodes/pnccontrollers/services/implementations/AppointmentSeriviceImplementation.java
@@ -1,5 +1,10 @@
 package com.kevocodes.pnccontrollers.services.implementations;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.kevocodes.pnccontrollers.domain.entities.Appointment;
@@ -15,10 +20,32 @@ public class AppointmentSeriviceImplementation implements AppointmentService{
     private final AppointmentRepository repository;
 
     @Override
-    public void RequestAppointment(Appointment requestedAppointment) {
+    public void requestAppointment(Appointment requestedAppointment) {
         repository.save(requestedAppointment);
     }
 
-    
-    
+    @Override
+    public List<Appointment> getAllAppointments() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void changeAppointmentStatus(UUID appointmentId) throws Exception{
+        Appointment appointment = repository.findById(appointmentId).orElseThrow();
+        appointment.setApproved(!appointment.getApproved());
+        repository.save(appointment);
+    }
+
+    @Override
+    public List<Appointment> getAllByDate(Date thisDate) {
+        return repository.getAppointmentsByDate(thisDate);
+    }
+
+    @Override
+    public void finishAppointment(UUID appointmentId) throws Exception{
+        Appointment appointment = repository.findById(appointmentId).orElseThrow();
+
+        appointment.setEndDate(Date.from(Instant.now()));
+        repository.save(appointment);
+    }
 }
