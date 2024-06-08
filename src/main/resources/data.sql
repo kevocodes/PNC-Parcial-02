27d00421992848e5aacafb0b6c855d148635b74f
Chriss6202 <00132720@uca.edu.sv>
2024-06-08 01:51:30 -0600

    add roles to sql :sparkles:

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-
 ALTER TABLE public."token" ALTER COLUMN active SET DEFAULT true;
 ALTER TABLE public."token" ALTER COLUMN "timestamp" SET DEFAULT CURRENT_TIMESTAMP;
 
 INSERT INTO "role" VALUES ('USER', 'user') ON CONFLICT (id) DO UPDATE set "name" = excluded."name";
-INSERT INTO "role" VALUES ('LBRN', 'librarian') ON CONFLICT (id) DO UPDATE set "name" = excluded."name";
+INSERT INTO "role" VALUES ('DCTR', 'doctor') ON CONFLICT (id) DO UPDATE set "name" = excluded."name";
+INSERT INTO "role" VALUES ('SCTR', 'secretary') ON CONFLICT (id) DO UPDATE set "name" = excluded."name";
 INSERT INTO "role" VALUES ('SUDO', 'sysadmin') ON CONFLICT (id) DO UPDATE set "name" = excluded."name";
 
-INSERT INTO "specialty" VALUES ('TEST', 'testjsjsjs') ON CONFLICT DO NOTHING;
+
